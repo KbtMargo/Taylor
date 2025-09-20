72fa97a81c7109e029c86a43e69c0508ae0edef6
KbtMargo <kubatiycom2@gmail.com>
2025-09-20 08:19:44 +0000

    feat(middleware): add QueryModeMiddleware

--- taylor/routes/web.php
+++ taylor/routes/web.php
@@ -4,4 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+}); 
+
+Route::middleware(['query.mode'])->group(function () {
+    Route::get('/smth', function () {
+        return 'Hello World)';
+    });
 });
