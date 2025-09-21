5f9faf27b0611c38a81473eed4917519bd6f64ad
KbtMargo <kubatiycom2@gmail.com>
2025-09-21 09:02:02 +0000

    deploy test error

--- resources/views/page/atelier.blade.php
+++ resources/views/page/atelier.blade.php
@@ -1,4 +1,4 @@
-{{-- @extends(view: 'layouts.app') --}}
+@extends('layouts.app')
 
 @section('title','Ательє | DressCode Website')
 
--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -1,4 +1,4 @@
-{{-- @extends('layouts.app') --}}
+@extends('layouts.app')
 
 @section('title','Головна | DressCode Website')
 
