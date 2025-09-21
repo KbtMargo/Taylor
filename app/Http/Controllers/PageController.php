9a63f692828ac2cea900a157e4a39eca84366613
KbtMargo <kubatiycom2@gmail.com>
2025-09-21 08:43:39 +0000

    deploy test error

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,10 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Log;
 
 class PageController extends Controller
 {
-    public function index()   { return view('page.index'); }
+    public function index()   { 
+        Log::error("HERE...");
+        return view('page.index'); 
+    }
     public function about()   { return view('page.about'); }
     public function atelier() { return view('page.atelier'); }
     public function faq()     { return view('page.faq'); }
--- resources/views/page/atelier.blade.php
+++ resources/views/page/atelier.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends(view: 'layouts.app') --}}
 
 @section('title','Ательє | DressCode Website')
 
