2f8991ca49decbe9b7a724a4c65cf08f36a48101
KbtMargo <kubatiycom2@gmail.com>
2025-09-21 12:23:06 +0000

    1

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 $public = __DIR__ . '/public';
-if ($uri !== '/' && file_exists($public . $uri)) return false;
-require $public . '/index.php';
+if ($uri !== '/' && file_exists($public . $uri)) { return false; }
+require $public . '/web.php';
