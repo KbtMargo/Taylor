dc3733ed74c7b844a5c797873eddfac1b8e07943
KbtMargo <kubatiycom2@gmail.com>
2025-09-21 08:17:55 +0000

    fixing

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
+Route::get('/', [PageController::class, 'index'])->name('home');
 
 Route::get('/page', [PageController::class, 'index'])->name('page.index');
 Route::get('/page/about', [PageController::class, 'about'])->name('page.about');
 Route::get('/page/atelier', [PageController::class, 'atelier'])->name('page.atelier');
 Route::get('/page/faq', [PageController::class, 'faq'])->name('page.faq');
+
+Route::fallback(fn() => response()->view('errors.404', [], 404));
